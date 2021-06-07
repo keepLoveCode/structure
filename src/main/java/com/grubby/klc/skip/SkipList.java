e4019d9652c295cd3a4e3ed9532df1ae6ddfebb3
grubby@lwork.com <grubby@lwork.com>
2021-06-07 18:04:55 +0800

    卡特兰数列

---
+++ src/main/java/com/grubby/klc/skip/SkipList.java
@@ -0,0 +1,100 @@
+package com.grubby.klc.skip;
+
+import java.util.Random;
+
+/**
+ * 跳表实现
+ *
+ * @author grubby
+ */
+public class SkipList {
+
+    private static final float SKIPLIST_P = 0.5f;
+    private static final int MAX_LEVEL = 16;
+
+    private int levelCount = 1;
+
+    private Node head = new Node();
+
+    public void insert(int value) {
+        int level = randomLevel();
+
+        Node newNode = new Node(level);
+        newNode.data = value;
+        Node[] update = new Node[level];
+
+        for (int i = 0; i < level; ++i) {
+            update[i] = head;
+        }
+
+        Node p = head;
+
+        for (int i = level - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        // in search path node next node become new node forwords(next)
+        for (int i = 0; i < level; ++i) {
+            newNode.forwards[i] = update[i].forwards[i];
+            update[i].forwards[i] = newNode;
+        }
+
+        // update node hight
+        if (levelCount < level) levelCount = level;
+    }
+
+
+    // 理论来讲，一级索引中元素个数应该占原始数据的 50%，二级索引中元素个数占 25%，三级索引12.5% ，一直到最顶层。
+    // 因为这里每一层的晋升概率是 50%。对于每一个新插入的节点，都需要调用 randomLevel 生成一个合理的层数。
+    // 该 randomLevel 方法会随机生成 1~MAX_LEVEL 之间的数，且 ：
+    //        50%的概率返回 1
+    //        25%的概率返回 2
+    //      12.5%的概率返回 3 ...
+    private int randomLevel() {
+        int level = 1;
+
+        while (Math.random() < SKIPLIST_P && level < MAX_LEVEL)
+            level += 1;
+        return level;
+    }
+
+    /**
+     * 跳表的节点，每个节点记录了当前节点数据和所在层数数据
+     */
+    public class Node {
+        private int data = -1;
+
+        /**
+         * 表示当前节点位置的下一个节点所有层的数据，从上层切换到下层，就是数组下标-1，
+         * forwards[3]表示当前节点在第三层的下一个节点。
+         */
+        private Node[] forwards = new Node[MAX_LEVEL];
+
+        /**
+         * 这个值其实可以不用，看优化insert()
+         */
+        private int maxLevel = 0;
+
+        public Node() {
+        }
+
+        public Node(int level) {
+            forwards = new Node[level];
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append("; levels: ");
+            builder.append(maxLevel);
+            builder.append(" }");
+            return builder.toString();
+        }
+    }
+
+}
