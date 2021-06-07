4346b0426d2e013a5fe8f1700f899260fafbdf93
grubby@lwork.com <grubby@lwork.com>
2021-06-07 16:39:56 +0800

    卡特兰数列

---
+++ src/main/java/com/grubby/klc/tree/Catalan.java
@@ -0,0 +1,14 @@
+package com.grubby.klc.tree;
+
+/**
+ * 卡特兰数列 C(n,2n) - C(n+1,2n) = C(n,2n) / n+1
+ *
+ * 设h(0) = 1 ,h(1) = 1;
+ *
+ * h(n) = h(0)*h(n-1) + h(1)*h(n-2) + ... +h(n-1)h(0)
+ *
+ * 出入栈，排列问题
+ * @author grubby
+ */
+public class Catalan {
+}
