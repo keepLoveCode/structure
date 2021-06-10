5c29b1da1df55e1ea8bdaca243139f969ee85fb0
grubby@lwork.com <grubby@lwork.com>
2021-06-10 17:36:04 +0800

    hashTable实现

--- src/main/java/com/grubby/klc/hashtable/HashMapLink.java
+++ src/main/java/com/grubby/klc/hashtable/HashMapLink.java
@@ -1,5 +1,7 @@
 package com.grubby.klc.hashtable;
 
+import java.util.Objects;
+
 /**
  * 拉链法 hashTable
  *
@@ -9,46 +11,144 @@ public class HashMapLink {
 
     private float factor = 0.75f;
 
-    private int[] hashTable;
+    private Node[] hashTable;
 
-    private int count = 0;
+    private int size = 0;
 
     private int capacity = 16;
 
+    private int threshold = 0;
 
     public HashMapLink() {
-        hashTable = new int[capacity];
-        count = 0;
+        hashTable = new Node[capacity];
+        size = 0;
+        threshold = (int) (capacity * factor);
     }
 
-    public void add(Integer value) {
-        Node node = new Node(value);
-        add(node);
+    public void put(Integer key, Integer value) {
+        Node node = new Node(key, value);
+        put(node);
     }
 
-    private void add(Node node) {
+
+    public int get(Integer key) {
+
+    }
+
+    private void put(Node node) {
         //查找key
-        // 有相同key则更新
-        // 无相同key，判断是否需要扩容
-        //扩容
-        // 查值
+        Node existsNode = searchNode(node);
+        if (Objects.isNull(existsNode)) {
+            insert(node);
+        } else {
+            existsNode.setValue(node.value);
+        }
     }
 
-    private int hashIndex(Integer value) {
-
+    private void insert(Node node) {
+        //插入
+        int index = indexOf(node);
+        if (hashTable[index] == null) {
+            hashTable[index] = node;
+        } else {
+            Node head = hashTable[index];
+            while (head.next != null) {
+                head = head.next;
+            }
+            head.next = node;
+        }
+        //先插入在扩容
+        if (++size > threshold) {
+            resize();
+        }
     }
 
-    private int hash(Integer value) {
-        return value;
+    private void resize() {
+        capacity = (capacity << 1);
+        threshold = (int) (capacity * factor);
+        Node[] newTable = new Node[capacity];
+        for (Node node : hashTable) {
+            Node p = node;
+            while (p != null) {
+                int index = indexOf(p);
+                if (newTable[index] == null) {
+                    newTable[index] = p;
+                } else {
+                    Node head = newTable[index];
+                    while (head.next != null) {
+                        head = head.next;
+                    }
+                    head.next = null;
+                }
+                Node swap = p;
+                p = p.next;
+                swap.next = null;
+            }
+        }
+        hashTable = newTable;
+    }
+
+    private Node searchNode(Node node) {
+        int index = indexOf(node);
+        if (hashTable[index] != null) {
+            Node head = hashTable[index];
+            while (head != null) {
+                if (head.equals(node)) {
+                    return head;
+                }
+                head = head.next;
+            }
+        }
+        return null;
+    }
+
+
+    private int indexOf(Node node) {
+        return node.hash & (capacity - 1);
     }
 
     public static class Node {
 
+        private Integer key;
+
         private Integer value;
+
+        private int hash;
 
         private Node next;
 
-        public Node(Integer value) {
+        public Node(Integer key, Integer value) {
+            this.value = value;
+            this.key = key;
+            this.hash = key;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+            Node node = (Node) object;
+            return Objects.equals(key, node.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return hash;
+        }
+
+        public Integer getKey() {
+            return key;
+        }
+
+        public void setKey(Integer key) {
+            this.key = key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
             this.value = value;
         }
     }
